62d00627a09217541873658a3e84d7af40deb8a7
Laetitia FORGEUR <l.forgeur@gmail.com>
2021-10-01 16:39:24 +0200

    Signup sécurisation

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -1,25 +1,28 @@
 const db = require("../models");
 const User = db.users;
 const bcrypt = require("bcrypt");
+const emailValidator = require("email-validator");
 
 // Register a new user
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10) // On hache le mot de passe 10 fois
-    .then((hash) => {
-      // On crée un nouvel utilisateur en utilisant notre schéma mongoose
-      const user = {
-        lastName: req.body.lastName,
-        firstName: req.body.firstName,
-        email: req.body.email,
-        // On récupère le mot de passe haché
-        password: hash,
-      };
-      // On sauvegarde ce nouvel utilisateur en bdd
-      User
-        .create(user)
-        .then(() => res.status(201).json({ message: 'User created' }))
-        .catch((error) => res.status(400).json({ error }))
-    })
-    .catch((error) => res.status(500).json({ error }))
+  if (emailValidator.validate(req.body.email)) {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = {
+          lastName: req.body.lastName,
+          firstName: req.body.firstName,
+          email: req.body.email,
+          // Get the hashed password
+          password: hash,
+        }
+        // Save this new user in database
+        User.create(user)
+          .then(() => res.status(201).json({ message: 'User created' }))
+          .catch((error) => res.status(400).json({ error }))
+      })
+      .catch((error) => res.status(500).json({ error }))
+  } else {
+    res.status(400).json({ message: 'Merci de saisir un email valide' }) 
+  }
 }
